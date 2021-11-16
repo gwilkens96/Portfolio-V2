07e2d0b378277c409ccc198a2add786563e57029
GriffinWilkens96 <gwilkens1234@gmail.com>
2021-11-16 17:49:27 -0500

    initial folders and mvc

---
+++ app.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import Header from './components/Header.js';
+import Navigation from './components/Navigation.js';
+import Project from './components/Project.js';
+import Footer from './components/Footer.js';
+
+function App() {
+    <useState />
+};
+
+export default App;
