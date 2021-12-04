93a6fe445523c6917a91490daf877797ef0dc279
GriffinWilkens96 <gwilkens1234@gmail.com>
2021-12-04 08:29:49 -0500

    Updates

--- src/app.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ProjectContainer from "./components/Project";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-const App = () => <ProjectContainer />;
-
-export default App;
